0e0ce7659dabaafebdf31e6fc14f2f4e0a436549
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-21 11:45:53 +0200

    first release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# profile
+
+
+<div align="center">  
+  <h1>profile</h1>
+</div>
+
+<div align="center">  
+<i>profile</i>
+</div>
+
+---
+
+<div align="center">
+<h4>Documentation</h4>
+</div>
+
+---
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/nosdav/profile/blob/gh-pages/LICENSE)
+[![npm](https://img.shields.io/npm/v/profile)](https://npmjs.com/package/profile)
+[![npm](https://img.shields.io/npm/dw/profile.svg)](https://npmjs.com/package/profile)
+[![Github Stars](https://img.shields.io/github/stars/nosdav/profile.svg)](https://github.com/nosdav/profile/)
+
+## Introduction
+
 profile
+
+## License
+
+- MIT
---
+++ app.js
@@ -0,0 +1,154 @@
+import { html, Component } from './js/spux.js'
+import { getPath, getQueryStringValue, loadFile, saveFile } from './util.js'
+import './css/style.css'
+import './js/dior.js'
+
+class UserProfile extends Component {
+  render() {
+    const { userPublicKey, name, picture, about, banner } = this.props;
+
+    return html`
+      <div class="user-profile">
+      ${banner ? html`<div class="banner"><img src="${banner}" alt="Banner" /></div>` : ''}
+      <br/>
+
+      
+      <h2>${name}</h2>
+
+        <img src="${picture}" alt="Profile Picture" class="user-picture" />
+        <p>Pubkey: ${userPublicKey}</p>
+        ${about ? html`<p>${about}</p>` : ''}
+      </div>
+    `;
+  }
+}
+
+class SocialLinks extends Component {
+  render() {
+    const { website, github } = this.props;
+
+    return html`
+      <div class="social-links">
+        <h3>Connect</h3>
+        ${website ? html`<a href="${website}" target="_blank">Website</a>` : ""}
+        ${github ? html`<br/><a href="https://github.com/${github}" target="_blank">Github</a>` : ""}
+        <br/><a href="https://facebook.com" target="_blank">Facebook</a>
+        <br/><a href="https://twitter.com" target="_blank">Twitter</a>
+        <br/><a href="https://instagram.com" target="_blank">Instagram</a>
+        <h3>Contacts</h3>
+      </div>
+    `
+  }
+}
+
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.fetchProfile = this.fetchProfile.bind(this);
+    const serverUrl = getQueryStringValue('storage') || 'https://nosdav.nostr.rocks';
+    const mode = getQueryStringValue('mode') || 'm';
+    const uri = getQueryStringValue('uri') || 'bookmarks.json';
+    this.state = {
+      userPublicKey: null,
+      filename: uri,
+      fileContent: '[]',
+      bookmarks: [],
+      newBookmarkUrl: '',
+      serverUrl: serverUrl,
+      mode: mode,
+    };
+  }
+
+
+  userLogin = async () => {
+    const userPublicKey = await window.nostr.getPublicKey();
+    console.log(`Logged in with public key: ${userPublicKey}`);
+    await this.setState({ userPublicKey: userPublicKey });
+    this.fetchProfile(userPublicKey, this.render)
+  }
+
+
+  async componentDidMount() {
+    await this.userLogin()
+    this.fetchProfile(this.state.userPublicKey, this.render.bind(this))
+  }
+
+  // fetchProfile.js
+  fetchProfile(pubkey, render) {
+    const NOSTR_RELAY_URL = 'wss://nostr-pub.wellorder.net'
+
+    let wss = new WebSocket(NOSTR_RELAY_URL)
+    let kind = 0
+    let id = 'profile'
+    wss.onopen = function () {
+      const req = `["REQ", "${id}", { "kinds": [${kind}], "authors": ["${pubkey}"] }]`
+      wss.send(req)
+    }
+
+    wss.onmessage = (msg) => {
+      const data = JSON.parse(msg.data)[2];
+      console.log(data);
+      const content = JSON.parse(data.content);
+
+      this.setState({
+        name: content.name,
+        picture: content.picture,
+        website: content.website,
+        about: content.about,
+        banner: content.banner,
+        github: content.identities?.[0]?.claim,
+      });
+
+      render();
+    };
+  }
+
+  render() {
+    const { userPublicKey, fileContent, name, picture, website, about, banner, github } = this.state;
+
+    return html`
+      <div id="container">
+
+        <div class="content">
+          <${UserProfile}
+            userPublicKey="${userPublicKey}"
+            name="${name}"
+            picture="${picture}"
+            about="${about}"
+            banner="${banner}"
+          />
+          <${SocialLinks}
+            website="${website}"
+            github="${github}"
+          />
+        </div>
+
+        <div class="file-section">
+          <label for="content">Notes</label>
+  
+          <textarea
+            id="file-content"
+            placeholder="Login First"
+            name="content"
+            rows="10"
+            value="${fileContent}"
+            onInput="${this.updateFileContent}"
+          ></textarea>
+          <div>
+            ${userPublicKey
+        ? html`
+                  <button id="save" onClick="${this.save}">Save</button>
+                  <button id="login" onClick="${this.userLogin}">
+                    Load
+                  </button>
+                `
+        : html` <button id="login" onClick="${this.userLogin}">
+                  Login
+                </button>`}
+          </div>
+        </div>
+      </div>
+    `
+  }
+}
