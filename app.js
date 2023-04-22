830f752975dbaabcbe2c68c8e61f87099c4ef39c
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-22 07:33:07 +0200

    move app to es6 module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { html, Component } from './js/spux.js'
+import { html, Component, render } from './js/spux.js'
 import { getPath, getQueryStringValue, loadFile, saveFile } from './util.js'
 // import './css/style.css'
 import './js/dior.js'
@@ -184,3 +184,6 @@ export class App extends Component {
     `
   }
 }
+
+render(html` <${App} /> `, document.body)
+
