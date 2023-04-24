2f2f43c412c22b7d3f62ecf7fac4028b1bdd9503
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-24 18:29:39 +0200

    remove underline in app links

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ class SocialLinks extends Component {
         ${github ? html`<br/><a href="https://github.com/${github}" target="_blank">Github</a>` : ""}
         ${twitter ? html`<br/><a href="https://twitter.com/${twitter}" target="_blank">Twitter</a>` : ""}
 
-        <h3>Apps <a target="_blank" href="https://nosdav.com/applist/?uri=${location.href}">🔗</a></h3>
+        <h3>Apps <a style="text-decoration:none" target="_blank" href="https://nosdav.com/applist/?uri=${location.href}">🔗</a></h3>
         ${apps.map(app => html`
           <a href="${app.uri}" target="_blank">${app.label || app.uri}</a><br/>
         `)}
